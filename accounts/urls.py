b45d64be1cbbc83dd7ed682b004cc93d0833f5cf
shs2810 <shs28100@naver.com>
2017-06-27 22:30:00 +0900

    회원가입/로그인/프로필/ 기능 1차

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,15 @@
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls import url
 from . import views
+
 urlpatterns = [
     url(r'^$', views.post),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', auth_views.login, name='login',
+        kwargs={'template_name' : 'accounts/login_form.html'}),
+    url(r'^logout/$', auth_views.logout, name='logout',
+        kwargs={'next_page' : settings.LOGIN_URL}),
+    url(r'^profile/$', views.profile, name='profile'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def post(request):
     return render(request, 'accounts/login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html',{
+                  'form':form,})
+
+def profile(request):
+    return render(request, 'accounts/profile.html')
