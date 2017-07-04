dd2d7f40375c66923e9acba470a15afbda3ab77b
shs2810 <shs28100@naver.com>
2017-07-04 17:27:19 +0900

    임시

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
-class LoginForm(AuthenticationForm):
+class SignupForm(UserCreationForm):
     pass
